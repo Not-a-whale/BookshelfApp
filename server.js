a9fc04d495623504fb3d52affb771dfa74ba7023
NikitaKornienko <nikita.kornienko@invogroup.com>
2021-01-29 15:06:33 +0200

    all

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const app = require("./backend/app");
+const app = require(express.static(`${__dirname}/backend/app`));
 
 const debug = require("debug")("node-angular");
 
