563a5777ab298401b262faf4e60290a4023e1219
NikitaKornienko <nikita.kornienko@invogroup.com>
2021-01-29 22:28:27 +0200

    all

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '..', 'dist', 'BookshelfApp')));
 
-app.get("/", function(req, resp){
- resp.sendFile(path.join(__dirname, '..', 'dist', 'BookshelfApp', 'index.html'));
-})
+//app.get("/", function(req, resp){
+// resp.sendFile(path.join(__dirname, '..', 'dist', 'BookshelfApp', 'index.html'));
+//})
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
